66995ec3cf7926e7c36518621c7b3effe4baff21
AJ <aj.zinkyaw@gmail.com>
2023-05-27 14:40:13 +0700

    fixed env not loaded and remove Ioc

--- bin/server.dart
+++ bin/server.dart
@@ -1,18 +1,14 @@
 import 'package:dox_core/dox_core.dart';
 import 'package:dox_sample/config/app.dart';
 import 'package:dox_sample/config/database.dart';
-import 'package:dox_sample/config/di.dart';
 
 void main() async {
   Config config = Config();
-
-  /// Initialize Postgres connection
-  Database().setup(config);
 
   /// Initialize Dox
   Dox dox = Dox();
   dox.initialize(config);
 
-  /// register Ioc injector
-  Ioc.register();
+  /// Initialize Postgres connection
+  Database().setup(config);
 }
