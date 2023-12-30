0760f24ae41dbac89e56f1f80b2913f0ac855416
(AJ) Zin Kyaw <aj.zinkyaw@gmail.com>
2023-12-30 14:26:24 +0630

    update docker to run on both production and development (#6)

--- lib/app/models/user/user.model.dart
+++ lib/app/models/user/user.model.dart
@@ -2,8 +2,20 @@ import 'package:dox_query_builder/dox_query_builder.dart';
 
 part 'user.model.g.dart';
 
-@DoxModel()
+@DoxModel(softDelete: true)
 class User extends UserGenerator {
   @override
   List<String> get hidden => <String>[];
+
+  @Column()
+  String? name;
+
+  @Column()
+  String? email;
+
+  @Column()
+  String? password;
+
+  @Column()
+  String? deletedAt;
 }
